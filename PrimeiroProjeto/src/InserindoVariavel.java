508f63508867024c615ac5aecab1122f2d1d1aa4
Bruno Garcia <bng.8@outlook.com>
2020-06-17 18:07:14 -0300

    Inseri uma nova classe

---
+++ PrimeiroProjeto/src/InserindoVariavel.java
@@ -0,0 +1,8 @@
+public class InserindoVariavel {
+    public static void main(String[]args){
+
+        int idade = 24;
+
+        System.out.print("A idade inserida na variavel é "+idade);
+    }
+}
