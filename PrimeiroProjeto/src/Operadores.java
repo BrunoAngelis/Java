2083f5f04e6e9018d1f761bf20efb7e502829928
Bruno Garcia <bng.8@outlook.com>
2020-06-17 19:21:56 -0300

    adicionei a classe de operadores

---
+++ PrimeiroProjeto/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main (String[]args){
+        int numero1 = 10;
+        int numero2 = 20;
+        int soma = numero1 + numero2;
+        System.out.print("O Soma de " +numero1+" + "+numero2+" é = "+ soma);
+
+    }
+}
