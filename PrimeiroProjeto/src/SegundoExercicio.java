35b745cedc01027a6809ad578ecac199e9c9ca25
Bruno Garcia <bng.8@outlook.com>
2020-06-17 19:09:23 -0300

    adicionei meu segundo exercicio

---
+++ PrimeiroProjeto/src/SegundoExercicio.java
@@ -0,0 +1,12 @@
+public class SegundoExercicio {
+    public static void main(String[] args) {
+        float salario = 1800;
+        String sexo = "masculino";
+        int idade = 24;
+        String estadoCivil = "solteiro";
+        String nome = "Bruno Garcia de Angelis";
+
+        System.out.print("O " + nome + " do sexo " + sexo + " idade " + idade + " Estado Civil " + estadoCivil +
+                " e salario " + salario + " encontra-se empregado neste estabelecimento.");
+    }
+}
