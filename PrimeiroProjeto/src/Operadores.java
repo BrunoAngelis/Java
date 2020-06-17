09d3cd124f590b26cfcf8c5407c6d295668700ee
Bruno Garcia <bng.8@outlook.com>
2020-06-17 19:51:44 -0300

    Adicionei as classes de Operadores

--- PrimeiroProjeto/src/Operadores.java
+++ PrimeiroProjeto/src/Operadores.java
@@ -1,9 +1,10 @@
 public class Operadores {
     public static void main (String[]args){
         int numero1 = 10;
-        int numero2 = 20;
+        int numero2 = 11;
         int soma = numero1 + numero2;
-        System.out.print("O Soma de " +numero1+" + "+numero2+" é = "+ soma);
+        System.out.print("Resto da divisão " + (numero2 % 2));
+
 
     }
 }
