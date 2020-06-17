44d67d22ace6d117e0669a2f3cf290717a3189ee
Bruno Garcia <bng.8@outlook.com>
2020-06-17 18:52:33 -0300

    Adicionei meu primeiro exercio

--- PrimeiroProjeto/src/InserindoVariavel.java
+++ PrimeiroProjeto/src/InserindoVariavel.java
@@ -2,7 +2,17 @@ public class InserindoVariavel {
     public static void main(String[]args){
 
         int idade = 24;
+        double salarioDouble = 3000;
+        float salarioFloat = 3000;
+        byte idadeByte = 12;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long numeroGrande = 1000L;
+        char caractere = '\u0041'; // 2 Bytes
 
-        System.out.print("A idade inserida na variavel é "+idade);
+        String nome = "Bruno Garcia de Angelis";
+
+        System.out.print(caractere);
     }
 }
