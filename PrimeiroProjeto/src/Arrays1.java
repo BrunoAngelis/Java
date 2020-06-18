a119b1994c3cece37d2d64dce37411fca30ea6dd
Bruno Garcia <bng.8@outlook.com>
2020-06-18 16:18:23 -0300

    Adicionei a classe de Arrays

---
+++ PrimeiroProjeto/src/Arrays1.java
@@ -0,0 +1,12 @@
+public class Arrays1 {
+    public static void main(String[] args) {
+        int [] idades = new int [3];
+        idades [0] = 20;
+        idades [1] = 23;
+        idades [2] = 15;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
