eb398333e6a31cedbd9e00d9c044b901c353c97c
Bruno Garcia <bng.8@outlook.com>
2020-06-18 02:57:47 -0300

    Alterei o looping para FOR

--- Livraria/src/CalculadoraDeEstoque.java
+++ Livraria/src/CalculadoraDeEstoque.java
@@ -1,12 +1,10 @@
 public class CalculadoraDeEstoque {
     public static void main(String[] args) {
-        double soma = 0;
-        int contador = 0 ;
 
-        while(contador < 35){
-            double valorDoLivro = 59.90;
-            soma += valorDoLivro;
-            contador ++;
+        double soma = 0;
+
+        for(int contador =0; contador <35 ; contador ++){
+            soma += 59.90;
         }
 
         System.out.println("O total do estoque é "+soma );
