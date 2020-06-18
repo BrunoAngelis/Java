00b6dac0daca79d742cd5533c06c5ab92bfec98d
Bruno Garcia <bng.8@outlook.com>
2020-06-18 17:14:02 -0300

    Adicionei 4 classes de Arrays

---
+++ PrimeiroProjeto/src/Arrays4.java
@@ -0,0 +1,9 @@
+public class Arrays4 {
+    public static void main(String[] args) {
+        String[]nomes = {"Bruno","Barbara","Maju","Lucinei","Nivaldo"};
+
+        for (String aux : nomes){
+            System.out.println(aux);
+        }
+    }
+}
