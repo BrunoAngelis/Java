b57604cfa816c267664f81d3729696b25ee78b6a
Bruno Garcia <bng.8@outlook.com>
2020-06-18 15:53:44 -0300

    Adicionei terceiro execicio do controle de fluxo

---
+++ PrimeiroProjeto/src/ControleFluxo4.java
@@ -0,0 +1,18 @@
+public class ControleFluxo4 {
+    public static void main(String[] args) {
+        int cont = 1;
+
+        while(cont <= 10){
+            System.out.println(cont);
+            cont++;
+        }
+
+        do{
+            System.out.println("Dentro do while");
+        }while (cont<10);
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("Valor de i é: "+i);
+        }
+    }
+}
---
+++ PrimeiroProjeto/src/ControleFluxo6.java
@@ -0,0 +1,15 @@
+public class ControleFluxo6 {
+    public static void main(String[] args) {
+
+        double valorCarro = 30000;
+
+        for (int parcela = (int)valorCarro; parcela >=1; parcela--){
+            double valorParcela =  valorCarro / parcela;
+            if (valorParcela <= 1000){
+                continue;
+            }
+             System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+
+        }
+    }
+}
