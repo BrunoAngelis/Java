c273f4709982cf0642241eb7b7ecf40bfb414f28
Bruno Garcia <bng.8@outlook.com>
2020-06-18 07:14:52 -0300

    Adicionei Terceiro Exercicio Controle de FLuxo com Switch

---
+++ PrimeiroProjeto/src/ExercicioControleFluxo2.java
@@ -0,0 +1,29 @@
+public class ExercicioControleFluxo2 {
+    public static void main(String[] args) {
+        byte dia = 1;
+
+        switch (dia){
+            case 1:
+                System.out.println("Doingo - Final de semana");
+                break;
+            case 2:
+                System.out.println("Doingo - Dia Util");
+                break;
+            case 3:
+                System.out.println("Doingo - Dia Util");
+                break;
+            case 4:
+                System.out.println("Doingo - Dia Util");
+                break;
+            case 5:
+                System.out.println("Doingo - Dia Util");
+                break;
+            case 6:
+                System.out.println("Doingo - Dia Util");
+                break;
+            case 7:
+                System.out.println("Doingo - Final de semana");
+                break;
+        }
+    }
+}
