661b0c99f5da8cfa34a15309776d0e16ab6388e8
Bruno Garcia <bng.8@outlook.com>
2020-06-22 00:14:00 -0300

    Adicionei modificadores de acesso Getters and Setters

--- JavaOnline/src/br/com/abc/javacore/introducaometodos/classes/Estudante.java
+++ JavaOnline/src/br/com/abc/javacore/introducaometodos/classes/Estudante.java
@@ -2,9 +2,10 @@ package br.com.abc.javacore.introducaometodos.classes;
 
 public class Estudante {
 
-    public String nome;
-    public int idade;
-    public double[] notas;
+    private String nome;
+    private int idade;
+    private double[] notas;
+    private boolean aprovado;
 
 
     public void exibirDados() {
@@ -33,11 +34,42 @@ public class Estudante {
         }
         media = media / notas.length;
         if (media >= 6) {
+            this.aprovado = true;
             System.out.println("Aprovado !!! Media " + media);
         } else {
+            this.aprovado = false;
             System.out.println("Reprovado !!! Media " + media);
         }
     }
 
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 
+    public void setIdade(int idade){
+        if (idade < 0){
+            System.out.println("Você não pode fazer isso!");
+            return;
+        }
+        this.idade = idade;
+    }
+
+    public void setNotas (double[]notas){
+        this.notas = notas;
+    }
+
+    public String  getNome (){
+        return this.nome;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public double [] getNotas(){
+        return this.notas;
+    }
+
+    public boolean isAprovado(){
+        return this.aprovado;
+    }
 }
--- JavaOnline/src/br/com/abc/javacore/introducaometodos/test/EstudanteTest.java
+++ JavaOnline/src/br/com/abc/javacore/introducaometodos/test/EstudanteTest.java
@@ -6,12 +6,12 @@ public class EstudanteTest {
     public static void main(String [] args){
         Estudante aluno1 = new Estudante();
 
-        aluno1.nome = "Bruno Garcia de Angelis";
-        aluno1.idade= 24;
-        //aluno1.notas = new double [] {9,6,6,10};
+        aluno1.setNome("Bruno Garcia de Angelis");
+        aluno1.setIdade(-1);
+        aluno1.setNotas(new double[]{3,4,5,});
         aluno1.exibirDados();
         aluno1.calcularMedia();
-
-
+        System.out.println(aluno1.getNome());
+        System.out.println("Aprovado? " + aluno1.isAprovado());
     }
 }
